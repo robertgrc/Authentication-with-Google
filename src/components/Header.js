65a5524e9cb259084342c3653c1017df1ef839b6
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-04-06 20:31:17 -0400

    components creation

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left"></div>
+      <div className="header__menu"></div>
+      <div className="header__right"></div>
+    </div>
+  );
+};
+
+export default Header;
